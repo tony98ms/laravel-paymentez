f4902e863d3e6c8a501042bc4ac8fd98e1ae7644
tonystore <thony918@outlook.com>
2024-12-17 22:08:17 -0500

    Creación de nuevo enum para el estado de respuesta de las transacciones

---
+++ src/Enums/StatusTransaction.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Blubear\LaravelPaymentez\Enums;
+
+enum StatusTransaction: string
+{
+    case SUCCESS = 'success';
+    case FAILURE = 'failure';
+    case PENDING = 'pending';
+
+    /**
+     * Get the label for the given status value.
+     *
+     * @return string
+     */
+
+    public function label(): string
+    {
+        return static::getLabel($this);
+    }
+
+    /**
+     * Get the label for the given status value.
+     *
+     * @param  self|string  $value
+     * @return string
+     */
+    public static function getLabel(self|string $value): string
+    {
+        return match ($value) {
+            self::PENDING => 'Pendiente',
+            self::SUCCESS => 'Exitoso',
+            self::FAILURE => 'Fallido',
+        };
+    }
+    /**
+     * Get the status from the given string value.
+     *
+     * @param  string  $value
+     * @return self
+     */
+    public static function fromString(string $value): self
+    {
+        return match ($value) {
+            'pending' => self::PENDING,
+            'success' => self::SUCCESS,
+            'failure' => self::FAILURE,
+        };
+    }
+}
